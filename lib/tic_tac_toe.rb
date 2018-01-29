001cb6cbb09bf730952320afb131db4b4adf089e
jared frankel <jaredfrankel10@gmail.com>
2018-01-29 16:02:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,9 +8,9 @@ WIN_COMBINATIONS = [
    [0,4,8],
    [2,4,6]
  ]
-  
+
  board = [" "," "," "," "," "," "," "," "," "]
- 
+
  def display_board(board)
    rows = ["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
    puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -19,23 +19,23 @@ WIN_COMBINATIONS = [
    puts rows[3]
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
  end
- 
+
  def input_to_index(user_input)
    user_input.to_i - 1
  end
- 
+
  def move(board, index, player)
    board[index] = player
  end
- 
+
  def valid_move?(board, index)
    position_taken?(board, index)  == false && index.between?(0, 8)
  end
- 
+
  def position_taken?(board, index)
     !(board[index].nil? || board[index] == " ")
  end
- 
+
  def turn(board)
    puts "Please enter 1-9:"
    user_input = gets.strip
@@ -47,33 +47,33 @@ WIN_COMBINATIONS = [
      turn(board)
    end
  end
- 
+
  def turn_count(board)
    board.count{|player| player == "X" || player == "O"}
  end
- 
+
  def current_player(board)
    turn_count(board).even? ? "X" : "O"
  end
- 
+
  def won?(board)
    WIN_COMBINATIONS.detect do |win|
     board[win[0]] == board[win[1]] && board[win[1]] == board[win[2]] && position_taken?(board, win[0])
   end
  end
- 
+
  def full?(board)
    board.all?{|i| i == "X" || i == "O"}
  end
- 
+
  def draw?(board)
    !won?(board) && full?(board)
  end
- 
+
  def over?(board)
    won?(board) || full?(board)
  end
- 
+
  def winner(board)
    if won?(board) && board[won?(board)[0]] == "X"
      return "X"
@@ -83,7 +83,7 @@ WIN_COMBINATIONS = [
    return nil
  end
  end
- 
+
  def play(board)
    while !over?(board) && !won?(board) && !draw?(board)
      turn(board)
